ba6a62faff5aa3c15d6ec90cb161a47f6dc2e6bc
Izuchukwu Kevin <kevinfizu@gmail.com>
2019-07-30 14:22:39 +0100

    Added telescope and fixed picture seeding
    Download laravel telescope and moved seeding of piture table into the seeding of its dependants

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,27 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): Void
     {
-        factory(User::class,20)->create();
-        factory(Store::class,20)->create();
-        factory(Post::class,100)->create();
-        factory(Picture::class,100)->create();
+        factory(User::class,20)->create()->each(function($user){
+            Picture::create([
+                'imageable_id' => $user->id,
+                'imageable_type' => 'App\User',
+                'filename' => 'img/users/'.time().'.png'
+            ]);
+        });
+        factory(Store::class,20)->create()->each(function($store){
+            Picture::create([
+                'imageable_id' => $store->id,
+                'imageable_type' => 'App\Store',
+                'filename' => 'img/stores/'.time().'.png'
+            ]);
+        });
+        factory(Post::class,100)->create()->each(function($post){
+            Picture::create([
+                'imageable_id' => $post->id,
+                'imageable_type' => 'App\Post',
+                'filename' => 'img/posts/'.time().'.png'
+            ]);
+        });
         factory(Category::class,20)->create();
     }
 }
